18c167efd95cfb5781496855b673f628d2f9adc6
JM <miggysvll@gmail.com>
2021-07-20 22:38:48 +0800

    new updates of frontend

---
+++ src/components/admin_dashboard/admin_dashboard_content/product_activator/product_activator_final.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <div style="margin-top: 30px; margin-bottom: 30px;" class="container">
+    <el-card shadow="always">
+
+    </el-card>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "product_activator_final"
+}
+</script>
+
+<style scoped>
+
+</style>
