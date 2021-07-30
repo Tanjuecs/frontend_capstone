06a210bb090c0424a955e5ac297373626f04b99d
JM <miggysvll@gmail.com>
2021-07-30 22:33:42 +0800

    change connection

--- src/store/0AuthRequest.js
+++ src/store/0AuthRequest.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 export default axios.create({
     // baseURL: 'https://capstonebackend20210621232104.azurewebsites.net', // live site
     
-    baseURL: 'https://localhost:44370',// local host site
-    // baseURL:'https://backendcapstone20210728232640.azurewebsites.net',
+    // baseURL: 'https://localhost:44370',// local host site
+    baseURL:'https://backendcapstone20210728232640.azurewebsites.net',
      headers: {
          "Content-type" : "application/json",
          "authapp" : "Basic:793bb6c2-4807-4805-a092-0a91d5ff62d7"
